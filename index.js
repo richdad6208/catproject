1d308e09ca817ace04bb88e8b2315b7550b31456
Jaesung Park <richdad6208@gmail.com>
2023-09-19 04:33:02 +0900

    fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config.js";
 import express from "express";
 import "./server/src/db.js";
-
+import path from "path";
 import apiRouter from "./server/src/routers/apiRouter.js";
 import globalRouter from "./server/src/routers/globalRouter.js";
 
@@ -12,6 +12,7 @@ app.listen(port, () => {
   console.log(`SERVER ON 🌏 in PORT: ${port}`);
 });
 
+app.use("/", express.static(path.join(__dirname, "/client/dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "1000kb" }));
 
